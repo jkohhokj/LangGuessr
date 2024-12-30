191018857b7aacdd9a33785db08eeff1422e2f05
jayden koh <jkohhokj@gmail.com>
2024-12-29 18:35:35 -0600

    decorate home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,175 +1,107 @@
 import Image from "next/image";
-import {getRandomGoogleLanguage} from "./languages";
-import sample_language1 from "@/app/Data/sample_language1.json";
-import { NewsResult} from "./google_news";
+import Link from "next/link";
+import { getRandomWikiLanguage } from "./languages";
 
 const Home = async () => {
-
-  const randomLanguage = getRandomGoogleLanguage();
-
-  let newsResults: NewsResult[] = [];
-  try {
-    // newsResults = await fetchNewsData(randomLanguage.language_code, apiKey); // UNCOMMENT TO USE REAL API
-    newsResults = sample_language1["news_results"];
-  } catch (error) {
-    console.error("Error fetching news data:", error);
+  interface Language {
+    language_code: string;
+    language_name: string;
   }
 
-  const tempChoicesSet = new Set<string>();
+  const fetchData = async (lang: Language) => {
+    try {
+      const baseUrl =
+        process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+      const response = await fetch(
+        `${baseUrl}/api/wiki?language_code=${lang.language_code}`
+      );
+      return response.text();
+    } catch {
+      return "gay";
+    }
+  };
+  async function getServerSideProps() {
+    const demoLanguage = await getRandomWikiLanguage();
+    const data = await fetchData(demoLanguage);
 
-  while (tempChoicesSet.size < 5) {
-    const selectedLanguage = getRandomGoogleLanguage();
-    tempChoicesSet.add(selectedLanguage.language_name);
+    return { props: { demoLanguage, data } };
   }
-
-  const tempChoices = Array.from(tempChoicesSet);
-  while (tempChoices.length < 5) {
-    const selectedLanguage = getRandomGoogleLanguage();
-    tempChoices.push(selectedLanguage.language_name);
-  }
-
-  const correctIndex = Math.floor(Math.random() * 6);
-  const optionChoices = tempChoices
-    .slice(0, correctIndex)
-    .concat(randomLanguage.language_name, tempChoices.slice(correctIndex));
-
+  const props = getServerSideProps();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <link
+        href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css"
+        rel="stylesheet"
+      />
+      <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-
         <div>
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Guess The Language
+          <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-300 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            LangGuessr: Guess The Language
           </h1>
-          <div>
-            <p>{randomLanguage.language_name}</p>
-            <p>{randomLanguage.language_code}</p>
-            <p>{randomLanguage.local_name}</p>
-          </div>
-          <ul>
-            {newsResults.map((news, correctIndex) => {
-              const titles = []; // To store all titles (highlight, stories, and normal titles)
-
-              // Extract the title from highlight
-              if (news.highlight?.title) {
-                titles.push(news.highlight.title);
-              }
-
-              // Extract titles from stories
-              if (news.stories) {
-                news.stories.forEach((story) => {
-                  if (story.title) {
-                    titles.push(story.title);
-                  }
-                });
-              }
-              if (news.title) {
-                titles.push(news.title);
-              }
-
-              // Render all titles (highlight and stories)
-              return (
-                <li key={correctIndex}>
-                  {titles.map((title, idx) => (
-                    <p key={idx}>{title}</p>
-                  ))}
-                </li>
-              );
-            })}
-          </ul>
-          <div>
-            {optionChoices.map((lang, index) => {
-              return (
-                <button
-                  key={index}
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-                >
-                  {lang}
-                </button>
-              );
-            })}
-          </div>
         </div>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="max-w-5xl">
+        <div className="flex justify-center">
+          
+          {(await props).props.demoLanguage.language_name}
+        </div>
+          <br />
+          <br />
+          {(await props).props.data}
+        </div>
+        <div>
+          <Link href={`/quiz-wiki`}>
+            <button className="animate-grow-shrink text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">
+              Play the game!
+            </button>
+          </Link>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href={"https://www.jkoh.dev/"}
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
+            src="https://nextjs.org/icons/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          About Me
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href={"https://github.com/jkohhokj/LangGuessr/"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Source Code
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href={"/resources"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Resources
         </a>
       </footer>
     </div>
